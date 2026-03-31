afaab9fcfbe20731d3b18e3616c0b5813648c7c4
Andra <andra.pintilescu@student.usv.ro>
2026-03-31 11:50:24 +0300

    initial commit

--- tema1_PIU/Program.cs
+++ tema1_PIU/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SalariuAngajat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oreLucrate = 0;
+            double tarifOra = 0;
+
+            Console.Write("Introduceti numarul de ore lucrate: ");
+            string ore = Console.ReadLine();
+
+            Console.Write("Introduceti tariful pe ora: ");
+            string tarif = Console.ReadLine();
+
+            int.TryParse(ore, out oreLucrate);
+            double.TryParse(tarif, out tarifOra);
+
+            double salariu = oreLucrate * tarifOra;
+
+            Console.WriteLine("Salariul este: " + salariu);
+
+            if (salariu > 3000)
+            {
+                Console.WriteLine("Salariu mare!");
+            }
+            else
+            {
+                Console.WriteLine("Ati lucrat prea putine ore pentru a avea un salariu mare!");
+            }
+        }
+    }
+}
